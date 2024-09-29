705b5c47d36ff34bbba8b405ad095352f95f84fd
Irvin Ignacio Cueto Lopez <iirvin.cueto.02@gmail.com>
2024-09-29 17:41:23 -0600

    formulario terminado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,55 @@
 @extends('layouts.app')
 
-@section('contenido')
 
+@section('titulo')
+    Registrate en DevStagram
 @endsection
 
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro de usuarios" srcset="">
+        </div>
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="">
+                <div class="mb-5">
+                    <label for="name" class="block uppercase text-gray-500 fond-bold">
+                        Nombre
+                    </label>
+                    <input id="name" name="name" type="text" placeholder="Tu nombre" class="border p-3 w-full">
+                </div>
 
+                <div class="mb-5">
+                    <label for="username" class="block uppercase text-gray-500 fond-bold">
+                        Username
+                    </label>
+                    <input id="username" name="username" type="text" placeholder="Tu Nombre de Usuario" class="border p-3 w-full">
+                </div>
 
+                <div class="mb-5">
+                    <label for="email" class="block uppercase text-gray-500 fond-bold">
+                        Email
+                    </label>
+                    <input id="email" name="email" type="email" placeholder="Tu Email de Registro" class="border p-3 w-full">
+                </div>
 
+                <div class="mb-5">
+                    <label for="password" class="block uppercase text-gray-500 fond-bold">
+                        Password
+                    </label>
+                    <input id="password" name="password" type="password" placeholder="Password de Restro" class="border p-3 w-full">
+                </div>
 
+                <div class="mb-5">
+                    <label for="password_confirmation" class="block uppercase text-gray-500 fond-bold">
+                        Password
+                    </label>
+                    <input id="password_confirmation" name="password_confirmation" type="password" placeholder="Password de Restro" class="border p-3 w-full">
+                </div>
 
+                <input type="submit" value="Crear Cuenta" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
 
+        </div>
+    @endsection
 
-
-
+    <div class="clas"></div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('principal');
-});
+Route::get('/', function () { return view('principal'); });
 
-Route::get('/crear-cuenta ', function () {
-    return view('auth.register');
-});
+Route::get('/crear-cuenta ', [RegisterController::class, 'index']);
